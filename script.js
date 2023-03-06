fad3ab4d728fbd7a727cf6f9dc8444a645bdfd2c
SALL Cheikh tidiane <cheikh-tidiane.sall@alstomgroup.com>
2023-03-06 15:36:55 -0500

    Add multiple grid lentgh, clear button and color picker

--- script.js
+++ script.js
@@ -1,24 +1,56 @@
-const container = document.querySelector('#grid-container'); // Get the container that will contain the divs
+function createGrid(gridLength) {
+    const container = document.querySelector('#grid-container'); // Get the container that will contain the divs
+    for (let i = 0; i < gridLength; i++) {
+        for (let j = 0; j < gridLength; j++){
+            const divGrid = document.createElement('div');
+            divGrid.classList.add('divGrid');
+            divGrid.style.width  = `${320/gridLength}px`;
+            divGrid.style.height = `${320/gridLength}px`;
+            container.appendChild(divGrid)
+        }
+    
+    }
+    const divs = document.querySelectorAll('.divGrid');
+    const clear = document.querySelector('.clearButton');
+    const color = document.querySelector('#colorpicker');
+    let colorValue = color.value;
+    color.addEventListener('mouseover', () => {
+        colorValue = color.value;;
 
-let gridLength = 64;
-for (let i = 0; i < gridLength; i++) {
-  for (let j = 0; j < gridLength; j++){
-    const divGrid = document.createElement('div');
-    divGrid.classList.add('divGrid');
-    divGrid.style.width  = "5px";
-    divGrid.style.height = "5px";
-    container.appendChild(divGrid)
-   }
-   
- }
-
- const divs = document.querySelectorAll('.divGrid');
-
- divs.forEach((div) => {
-
-    // and for each one we add a 'mouseover' listener
-    div.addEventListener('mouseover', (e) => {
-        div.style.background = "black";
-  
     });
-  });
+    
+    divs.forEach((div) => {
+   
+       // and for each one we add a 'mouseover' listener
+       div.addEventListener('mouseover', () => {
+           div.style.background = colorValue;
+            
+       });
+
+       clear.addEventListener('click', () => {
+        div.style.background = "white";
+  
+        });
+     });
+
+}
+
+createGrid(16);
+let slider = document.getElementById("gridRange");
+let output = document.getElementById("value");
+output.textContent = slider.value;
+
+slider.oninput = function() {
+  output.textContent = this.value + "  X " + this.value ;
+}
+let gridLength = slider.value;
+
+slider.addEventListener('mouseup', function() {
+    const container = document.querySelector('#grid-container');
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+      }
+    createGrid(slider.value);
+});
+
+
