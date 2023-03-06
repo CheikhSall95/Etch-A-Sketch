01c5f31cbc4831f27a2e060f8b2eac0d1c852137
SALL Cheikh tidiane <cheikh-tidiane.sall@alstomgroup.com>
2023-03-06 14:29:44 -0500

    Parameterize the grid

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const container = document.querySelector('#grid-container'); // Get the container that will contain the divs
 
-let gridLength = 16;
+let gridLength = 64;
 for (let i = 0; i < gridLength; i++) {
   for (let j = 0; j < gridLength; j++){
     const divGrid = document.createElement('div');
     divGrid.classList.add('divGrid');
-    divGrid.style.width  = "20px";
-    divGrid.style.height = "20px";
+    divGrid.style.width  = "5px";
+    divGrid.style.height = "5px";
     container.appendChild(divGrid)
    }
    
